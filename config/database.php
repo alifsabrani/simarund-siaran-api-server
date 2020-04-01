2173bece7237f509ed5bf8430d8ad77ba2284786
alifsabrani <alifsabrani@gmail.com>
2020-04-01 13:16:13 +0800

    add db config

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => 'https://simarund-tvri-ntb.000webhostapp.com',
+            'port' => '3306',
+            'database' => 'id13107001_simarund',
+            'username' => 'id13107001_root',
+            'password' => '^kjsD|*JQo29X?!c',
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
